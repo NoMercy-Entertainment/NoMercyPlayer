7f327626d3480b6cb00503453e43ecd8b52a91be
Stoney Eagle <45034970+StoneyEagle@users.noreply.github.com>
2023-08-16 18:01:43 +0200

    chore: eslint fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,42 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
+// noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-    base: "./",
-    publicDir: resolve(__dirname, 'public'),
-    build: {
-        sourcemap: false,
-        rollupOptions: {
-            output: {
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`,
-                manualChunks: {
-                    base: ['./src/base.ts'],
-                    ui: ['./src/ui.ts'],
-                    buttons: ['./src/buttons.ts'],
-                    functions: ['./src/functions.ts'],
-                    keyEvents: ['./src/keyEvents.ts'],
-                    secrets: ['./src/secrets.ts'],
-                },
-            },
-        },
-    },
-    server: {
-        port: 5501,
-        host: '0.0.0.0',
-        hmr: {
-            port: 5501,
-            protocol: 'wss',
-            host: 'vscode.nomercy.tv',
-            clientPort: 443,
-            path: '/vite/',
-        },
-    },
-    preview: {
-        port: 5501,
-        host: '0.0.0.0',
-    },
-    clearScreen: true,
+	base: './src/',
+	publicDir: resolve(__dirname, 'public'),
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				entryFileNames: 'assets/[name].js',
+				chunkFileNames: 'assets/[name].js',
+				assetFileNames: 'assets/[name].[ext]',
+				manualChunks: {
+					base: ['./src/base.ts'],
+					ui: ['./src/ui.ts'],
+					buttons: ['./src/buttons.ts'],
+					functions: ['./src/functions.ts'],
+					keyEvents: ['./src/keyEvents.ts'],
+					secrets: ['./src/secrets.ts'],
+				},
+			},
+		},
+	},
+	server: {
+		port: 5501,
+		host: '0.0.0.0',
+		hmr: {
+			port: 5501,
+			protocol: 'wss',
+			host: 'vscode.nomercy.tv',
+			clientPort: 443,
+			path: '/vite/',
+		},
+	},
+	preview: {
+		port: 5501,
+		host: '0.0.0.0',
+	},
+	clearScreen: true,
 });
